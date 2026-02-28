b37c0fe5c44f31e82668616dea060acc9de70231
Richardas Keršis <richardas.kersis@gmail.com>
2026-02-28 13:57:08 +0200

    Aspect ratio supporting any screen

--- contents/code/main.js
+++ contents/code/main.js
@@ -11,13 +11,27 @@
 // Configuration area.
 
 // Coefficient is the percentage of current screen size that the window size would become.
-coefficient = 0.67;
+// Default is 2/3 (two-thirds);
+const coefficient = 0.67;
+// Aspect ratios of the new window size for width : height. Default is 16:9.
+const widthAspectRatio = 16;
+const heightAspectRatio = 9;
 
 // End of configuration area.
 
 const main = () => {
     if (coefficient < 0 || coefficient > 1) {
       print("Coefficient variable has a wrong value, should a float value in range [0 ... 1]");
+      return;
+    }
+
+    if (widthAspectRatio < 1) {
+      print("widthAspectRatio must not be less than 1");
+      return;
+    }
+
+    if (heightAspectRatio < 1) {
+      print("heightAspectRatio must not be less than 1");
       return;
     }
 
@@ -43,15 +57,28 @@ const main = () => {
 
     // Goal and calculation logic explanation:
     // I want to set the size of the active window to 2/3 of the current screen and place it in the center.
+    // The new width should be based on the monitor height (so that ultrawide monitors have adequate window size).
+    // And the new window should be of 16:9 aspect ratio.
     // I have x, y (top-left corner position of the window), its width and height.
     // What do:
-    // * width and height would be: screen width * coefficient, which is 2/3. Same for the height.
-    // * position x and y would be: screen's x or y plus the remaining free space of the screen (1/3 of the screen width) divided by 2.
+    // * height would be: screen_height * coefficient, which is 2/3.
+    // * width would be: new_height * 16 / 9.
+    // * position x: `screen_x + screen_width / 2 - window_width / 2`. And same for position y.
+    //   Explanation:
+    //     * screen_x: screen's own position. If you have two FHD (1920 x 1080) screens and you're on the right one, right one's x position will start at 1920, not 0.
+    //     * screen_width / 2: middle of the screen.
+    //     * window_width / 2: middle of the window.
     const desiredGeometry = cloneObject(workspace.activeWindow.frameGeometry);
-    desiredGeometry.width = workspace.activeScreen.geometry.width * coefficient;
-    desiredGeometry.height = workspace.activeScreen.geometry.height * coefficient;
-    desiredGeometry.x = workspace.activeScreen.geometry.x + workspace.activeScreen.geometry.width * (1 - coefficient) / 2
-    desiredGeometry.y = workspace.activeScreen.geometry.y + workspace.activeScreen.geometry.height * (1 - coefficient) / 2
+    const isScreenHorizontal = workspace.activeScreen.geometry.width > workspace.activeScreen.geometry.height;
+    if (isScreenHorizontal) {
+      desiredGeometry.height = workspace.activeScreen.geometry.height * coefficient;
+      desiredGeometry.width = desiredGeometry.height * widthAspectRatio / heightAspectRatio;
+    } else {
+      desiredGeometry.width = workspace.activeScreen.geometry.width * coefficient;
+      desiredGeometry.height = desiredGeometry.width * heightAspectRatio / widthAspectRatio;
+    }
+    desiredGeometry.x = workspace.activeScreen.geometry.x + (workspace.activeScreen.geometry.width / 2) - (desiredGeometry.width / 2)
+    desiredGeometry.y = workspace.activeScreen.geometry.y + (workspace.activeScreen.geometry.height / 2) - (desiredGeometry.height / 2)
 
     // workspace.activeWindow.maximizeMode
     // * 0 = floating window.
